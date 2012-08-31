6cfac348cf05ab5d8c5aec381b9a9e3b188c31e0
Nicola Racco <nicola@nicolaracco.com>
2012-08-31 10:46:45 +0200

    connection status

--- app.rb
+++ app.rb
@@ -2,15 +2,28 @@
 require 'bundler/setup'
 require 'SocketIO'
 
-host = 'aqueous-sea-6000.herokuapp.com'
+host = 'localhost'
 port = 5000
+
+connected = false
+
 client = SocketIO.connect("http://#{host}:#{port}") do
   before_start do
+    on_connect do
+      connected = true
+      puts "[STATUS] connection established"
+    end
     on_message do |message|
       puts "incoming message: #{message}"
     end
     on_event 'news' do |data|
       puts data.first # data is an array fo things.
     end
+    on_error do
+      puts 'error'
+    end
   end
 end
+
+connected = false
+puts "[STATUS] connection lost"
