4b43f49a977c2b96bd1cd03a37e2b1dcb1f25406
Stefano Guglielmetti <stefano@mikamai.com>
2012-09-03 16:53:57 +0200

    server changed to unbit

--- app.rb
+++ app.rb
@@ -5,8 +5,11 @@ Bundler.require
 require 'eventmachine'
 require 'em-http-request'
 
+puts "client running"
+
 EventMachine.run {
-  http = EventMachine::HttpRequest.new("ws://localhost:9292/device").get :timeout => 0
+  #http = EventMachine::HttpRequest.new("ws://localhost:9292/device").get :timeout => 0
+  http = EventMachine::HttpRequest.new("ws://rpi.unbit.it/").get :timeout => 0
   http.errback { puts "oops" }
   http.callback {
       puts "WebSocket connected!"
